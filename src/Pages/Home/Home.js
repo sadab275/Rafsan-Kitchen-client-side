1cab7482e374c2d93ed10b1e05c0f54605e11271
Sadab <sadabrahman275@gmail.com>
2022-11-08 21:24:21 +0600

    services creating

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import About from './About';
 import Banner from './Banner';
 import Chef from './Chef';
+import Services from './Services/Services';
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <About></About>
+            <Services></Services>
             <Chef></Chef>
         </div>
     );
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className='text-2xl font-bold text-orange-300'>Services</p>
+                <h2 className="text-5xl font-semibold">My Food Menu</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
